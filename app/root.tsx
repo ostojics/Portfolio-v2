2fe3c76938ac00df27f3fa97bfde93ef181fed4b
ostojics <slobodan.ostojic@axiomq.com>
2022-02-07 21:25:43 +0100

    Added prettier

--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,8 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from 'remix';
+import type {MetaFunction} from 'remix';
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return {title: 'New Remix App'};
 };
 
 export default function App() {
@@ -25,7 +18,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
   );
