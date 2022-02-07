6c701670bff31e79e1da662e6b484932e75e821d
ostojics <slobodan.ostojic@axiomq.com>
2022-02-07 21:55:10 +0100

    Added scss

--- app/root.tsx
+++ app/root.tsx
@@ -1,8 +1,14 @@
-import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from 'remix';
+import {Links, LinksFunction, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from 'remix';
 import type {MetaFunction} from 'remix';
+
+import styles from './styles/main.css';
 
 export const meta: MetaFunction = () => {
   return {title: 'New Remix App'};
+};
+
+export const links: LinksFunction = () => {
+  return [{rel: 'stylesheet', href: styles}];
 };
 
 export default function App() {
