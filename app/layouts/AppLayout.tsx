e1623b43b5f3b2001460166aff855a891f4c6946
ostojics <slobodan.ostojic@axiomq.com>
2022-02-08 20:26:10 +0100

    Added spoken languages and footer

--- app/layouts/AppLayout.tsx
+++ app/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Divider from '~/components/Divider/Divider';
 import Text from '~/components/Text/Text';
 
 const AppLayout: React.FC = ({children}) => {
@@ -13,7 +14,20 @@ const AppLayout: React.FC = ({children}) => {
             <Text tag="span">Web Developer</Text>
           </div>
         </div>
-        <div className="app-layout__profile-content">content</div>
+        <div className="app-layout__profile-content">
+          <div className="app-layout__profile-languages">
+            <Text tag="h3">Spoken languages</Text>
+            <div className="app-layout__languages-container">
+              <div className="app-layout__profile-language">
+                <Text tag="h5">Serbian</Text>
+              </div>
+              <div className="app-layout__profile-language">
+                <Text tag="h5">English</Text>
+              </div>
+            </div>
+          </div>
+          <Divider />
+        </div>
         <div className="app-layout__profile-footer">
           <a href="https://github.com/ostojics" target="_blank" rel="noopener noreferrer">
             <i className="ion-social-github" />
