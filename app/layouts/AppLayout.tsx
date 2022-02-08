1cc42de8fd5378be45875847d15f355d34616bc0
ostojics <slobodan.ostojic@axiomq.com>
2022-02-08 19:36:23 +0100

    Added profile layout

--- app/layouts/AppLayout.tsx
+++ app/layouts/AppLayout.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Text from '~/components/Text/Text';
 
 const AppLayout: React.FC = ({children}) => {
   return (
     <div className="app-layout">
-      <div className="app-layout__profile">profile</div>
+      <div className="app-layout__profile">
+        <div className="app-layout__profile-head">
+          <div className="app-layout__profile-head-text">
+            <img src="/assets/maturska.jpg" alt="profile" />
+            <Text tag="h3">Slobodan Ostojić</Text>
+            <Text tag="span">Web Developer</Text>
+          </div>
+        </div>
+        <div className="app-layout__profile-content">content</div>
+        <div className="app-layout__profile-footer">
+          <a href="https://github.com/ostojics" target="_blank" rel="noopener noreferrer">
+            <i className="ion-social-github" />
+          </a>
+          <a href="https://github.com/ostojics" target="_blank" rel="noopener noreferrer">
+            <i className="ion-android-mail" />
+          </a>
+          <a href="https://www.linkedin.com/in/slobodan-ostojić-7006101b4/" target="_blank" rel="noopener noreferrer">
+            <i className="ion-social-linkedin" />
+          </a>
+        </div>
+      </div>
       <div className="app-layout__content">{children}</div>
     </div>
   );
